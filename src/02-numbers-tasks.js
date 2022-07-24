494dc75fa1687c5a3483ce225cbcba0ad92af49f
Nikolay <nikolay.kovzik@gmail.com>
2022-07-24 15:16:05 +0300

    feat: solve half of array tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -141,8 +141,8 @@ function getLastDigit(value) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return +value;
 }
 
 /**
@@ -158,8 +158,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt((a ** 2) + (b ** 2) + (c ** 2));
 }
 
 
@@ -201,8 +201,20 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 1) {
+    return false;
+  }
+  let divider = 2;
+  let isNumPrime = true;
+  while (divider ** 2 <= n && isNumPrime) {
+    if (n % divider === 0) {
+      isNumPrime = false;
+    } else {
+      divider += 1;
+    }
+  }
+  return isNumPrime;
 }
 
 /**
@@ -221,6 +233,7 @@ function isPrime(/* n */) {
  *   toNumber(new Number(42), 0) => 42
  */
 function toNumber(/* value, def */) {
+  // return value === undefined || Number.isNaN(+value) ? def : value;
   throw new Error('Not implemented');
 }
 
